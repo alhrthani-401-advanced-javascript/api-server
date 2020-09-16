b47a888d50927a8477c7b6f09f2c897e6e8c957d
AhmadHirthani <hertani86@gmail.com>
2020-09-16 18:18:39 +0300

    finishClass-09

--- __tests__/404.test.js
+++ __tests__/404.test.js
@@ -4,12 +4,12 @@ const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('404 module', () => {
-    it('404 not found', () => {
-        return mockRequest.get("/not found").then(result=>{
-            expect(result.err).toHaveBeenCalled();
-            expect(result.status).toBe(404);
-        }).catch(err=> {
-            console.log(err);
-        });
+  it('404 not found', () => {
+    return mockRequest.get('/not found').then(result=>{
+      expect(result.err).toHaveBeenCalled();
+      expect(result.status).toBe(404);
+    }).catch(err=> {
+      console.log(err);
     });
+  });
 });
--- routes/category.js
+++ routes/category.js
@@ -16,45 +16,45 @@ categoryRouter.delete('/categories/:id', deleteCategory);
 
 
 function getCategory(req, res, next) {
-    const id = req.params.id;
-    category.get(id).then(data => {
-        res.status(200).json(data);
-    }).catch(next);
+  const id = req.params.id;
+  category.get(id).then(data => {
+    res.status(200).json(data);
+  }).catch(next);
 }
 
 function postCategory(req, res, next) {
-    console.log("req.body >>> ", req.body)
-    category.create(req.body).then(data => {
-        res.status(201).json(data);
-    }).catch(err => {
-        // or do .catch(next) like the getFood function
-        console.log(err);
-        next(err);
-    });
+  console.log('req.body >>> ', req.body);
+  category.create(req.body).then(data => {
+    res.status(201).json(data);
+  }).catch(err => {
+    // or do .catch(next) like the getFood function
+    console.log(err);
+    next(err);
+  });
 }
 
 function putCategory(req, res, next) {
-    console.log("req.body >>> ", req.body)
-    category.update(req.params.id, req.body).then(data => {
-        res.status(201).json(data);
-    }).catch(err => {
-        // or do .catch(next) like the getFood function
-        console.log(err);
-        next(err);
-    });
+  console.log('req.body >>> ', req.body);
+  category.update(req.params.id, req.body).then(data => {
+    res.status(201).json(data);
+  }).catch(err => {
+    // or do .catch(next) like the getFood function
+    console.log(err);
+    next(err);
+  });
 }
 
 function deleteCategory(req, res, next) {
-    console.log("req.body >>> ", req.body)
-    category.delete(req.params.id).then(data => {
-        res.status(201).json(data);
-    }).catch(err => {
-        // or do .catch(next) like the getFood function
-        console.log(err);
-        next(err);
-    });
+  console.log('req.body >>> ', req.body);
+  category.delete(req.params.id).then(data => {
+    res.status(201).json(data);
+  }).catch(err => {
+    // or do .catch(next) like the getFood function
+    console.log(err);
+    next(err);
+  });
 }
 
-module.exports
+module.exports;
 
 module.exports = categoryRouter;
